93bc2a0af90db1e1727cac560c8c0acb7dc5bc14
Matt Filetto <matt.filetto@gmail.com>
2015-11-17 01:11:27 -0800

    remove prebuilt GPS HAL
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- klte/klte-vendor-blobs.mk
+++ klte/klte-vendor-blobs.mk
@@ -20,10 +20,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/klte/proprietary/etc/flp.conf:system/etc/flp.conf \
     vendor/samsung/klte/proprietary/etc/gps.conf:system/etc/gps.conf \
     vendor/samsung/klte/proprietary/etc/sap.conf:system/etc/sap.conf \
-    vendor/samsung/klte/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
-    vendor/samsung/klte/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
-    vendor/samsung/klte/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
-    vendor/samsung/klte/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
     vendor/samsung/klte/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/samsung/klte/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/klte/proprietary/lib/libperipheral_client.so:system/lib/libperipheral_client.so \
